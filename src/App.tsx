640af27dd60aa4e1baf386c09f70db84f6bccb44
Dérick <6derick6@gmail.com>
2022-12-08 21:23:27 -0300

    Criação da label da tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IFormState {
+  email: string;
+  password: string;
 }
 
+const App = () => {
+
+  const [formState, setFormState] = useState<IFormState>({
+    email: "",
+    password: "",
+  });
+
+
+  const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {email, password} = formState;
+
+    if(!email || !password) {
+      return;
+    }
+
+  },[]);
+
+
+  return (
+    <div className='container mt-5'>
+      <p className='fs-1 mb-5'>Login</p>
+
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">E-mail</label>
+          <input 
+            type="email" 
+            className="form-control" 
+            id="email" name="email" 
+            required 
+            value={formState.email}
+            onChange={(event) => 
+              setFormState({
+                ... formState,
+                email: event.currentTarget?.value || "",
+              })
+            }
+            />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Senha</label>
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password" 
+            name="password" 
+            required 
+            value={formState.password}
+            onChange={(event) => 
+              setFormState({
+                ... formState,
+                password: event.currentTarget?.value || "",
+              })
+            }
+            />
+        </div>
+        <button type="submit" className="btn btn-primary">Entrar</button>
+      </form>
+
+    </div>
+  );
+};
+
 export default App;
+
+
